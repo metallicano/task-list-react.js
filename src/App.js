d859aad9fafbeb22f24f6860d2810c381f4a769f
daniel <danielmtgiraldo@gmail.com>
2020-05-08 17:41:57 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { TaskRow } from './components/TaskRow'
+import {TaskBanner} from './components/TaskBanner'
+import {TaskCreator} from './components/TaskCreator'
+
 
 function App() {
+
+  const [userName, setUserName] = useState('Daniel');
+  const [taskItems, setTaskItems] = useState([
+    { name: 'Task one', done: false },
+    { name: 'Task two', done: false },
+    { name: 'Task three', done: true },
+    { name: 'Task four', done: false }
+  ])
+
+  
+  const createNewTask = taskName =>{
+    if (!taskItems.find(t => t.name === taskName)){
+      setTaskItems([...taskItems, {name: taskName, done: false}])
+  }
+  } 
+
+
+  const toggleTask = task =>
+  setTaskItems(taskItems.map(t =>(t.name === task.name ? {...t,done: !t.done} : t)))
+
+  const taskTableRows = () => taskItems.map(task => (
+    <TaskRow task={task} key={task.name} toggleTask={toggleTask}></TaskRow>
+  ))
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <TaskBanner userName = {userName} taskItems={taskItems}></TaskBanner>
+     <TaskCreator callback = {createNewTask} ></TaskCreator>
+      <table className ='table table-striped table-bordered'>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Done</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {taskTableRows()}
+
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/components/TaskBanner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const TaskBanner = props =>(
+    <h4 className = "bg-primary text-white text-center p-4">
+        {props.userName}'s Task App ({props.taskItems.filter(t=>!t.done).length} task to do)
+
+    </h4>
+);
